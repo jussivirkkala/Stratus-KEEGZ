ddffde3cdfa3e4947c97cce3e92114a9e09602f0
Jussi Virkkala <jussivirkkala@hotmail.com>
2023-11-19 18:00:08 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿/* Displaying stratuseeg.com patient information from .keegz or StaticInfo.xml 
  * @jussivirkkala
- * 2023-11-19 v.1.08 .NET8 and AOT.
+ * 2023-11-19 v1.0.8 .NET8 and AOT.
  * 2023-03-16 v1.0.7 .NET7 added stratuseeg.com
  * 2022-04-19 v1.0.4 .NET6 removed namespace, class, main
  * 2021-09-02 v1.0.3 Exclude file list, visit 
@@ -24,10 +24,8 @@ using System.Reflection;
 
 string r = "";
 r += Line("Displaying stratus.eeg patient information from .keegz or StaticInfo.xml v1.0.8" + 
-
 //    FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion +
 //    FileVersionInfo.GetVersionInfo(System.AppContext.BaseDirectory).FileVersion +
-
     "\ngithub.com/jussivirkkala/Stratus-keegz");
 
 if (args.Length!=1)
